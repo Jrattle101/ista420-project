66dda760048d105d6baf273a82cae8979a7fbf8d
Jason Adams <jason.r.adams@outlook.com>
2017-06-26 11:56:31 -0400

    Upload user docs
    uploaded user docs and fixed iteration3 erd

--- Implementation/interation03-implementation.sql
+++ Implementation/interation03-implementation.sql
@@ -8,7 +8,7 @@
 --WHERE referenced_object_id = object_id('teams')
 
 
-USE [ista420-iteration02];
+USE [ista420];
 
 DROP TABLE if exists teams;
 
@@ -28,65 +28,74 @@ DROP TABLE if exists roster;
 
 CREATE TABLE roster (
 	PlayerID INTEGER NOT NULL PRIMARY KEY,
-	TeamID INTEGER NOT NULL FOREIGN KEY REFERENCES teams(TeamID),
+	TeamID INTEGER NOT NULL,-- FOREIGN KEY REFERENCES teams(TeamID),
 	FirstName TEXT NOT NULL,
 	LastName TEXT NOT NULL,
 	Position TEXT,
 	JerseyNum INTEGER NOT NULL,
 
 	);
-	
-INSERT INTO roster 
-	VALUES	-- Cubs
-			(101, 1, 'J.', 'Baez', '2B', 11),
-			(102, 1, 'K.', 'Bryant', '3B', 12),
-			(103, 1, 'W.', 'Contreras', 'C', 13),
-			(104, 1, 'D.', 'Fowler', 'CF', 14),
-			(105, 1, 'A.', 'Rizzo', '1B', 15),
-			(106, 1, 'A.', 'Russell', 'SS', 16),
-			(107, 1, 'J.', 'Heyward', 'RF', 17),
-			(108, 1, 'C.', 'Coghlan', 'LF', 18),
-			(109, 1, 'J.', 'Lester', 'SP', 19),
+bulk insert roster from 'C:\Users\sintal\Desktop\Project\Bulk\roster.txt'
+with 
+(
+firstrow=2,
+datafiletype='char',
+fieldterminator=',',
+rowterminator='\n'
+);
+
+
+--INSERT INTO roster 
+--	VALUES	-- Cubs
+			--(101, 1, 'J.', 'Baez', '2B', 11),
+			--(102, 1, 'K.', 'Bryant', '3B', 12),
+			--(103, 1, 'W.', 'Contreras', 'C', 13),
+			--(104, 1, 'D.', 'Fowler', 'CF', 14),
+			--(105, 1, 'A.', 'Rizzo', '1B', 15),
+			--(106, 1, 'A.', 'Russell', 'SS', 16),
+			--(107, 1, 'J.', 'Heyward', 'RF', 17),
+			--(108, 1, 'C.', 'Coghlan', 'LF', 18),
+			--(109, 1, 'J.', 'Lester', 'SP', 19),
 			
-			-- Indians
-			(201, 2, 'R.', 'Davis', 'CF', 21),
-			(202, 2, 'J.', 'Kipnis', '2B', 22),
-			(203, 2, 'F.', 'Lindor', 'SS', 23),
-			(204, 2, 'M.', 'Napoli', '1B', 24),
-			(205, 2, 'R.', 'Perez', 'C', 25),
-			(206, 2, 'J.', 'Ramirez', '3B', 26),
-			(207, 2, 'L.', 'Chisenhall', 'RF', 27),
-			(208, 2, 'C.', 'Crisp', 'LF', 28),
-			(209, 2, 'C.', 'Kluber', 'SP', 29),
+			---- Indians
+			--(201, 2, 'R.', 'Davis', 'CF', 21),
+			--(202, 2, 'J.', 'Kipnis', '2B', 22),
+			--(203, 2, 'F.', 'Lindor', 'SS', 23),
+			--(204, 2, 'M.', 'Napoli', '1B', 24),
+			--(205, 2, 'R.', 'Perez', 'C', 25),
+			--(206, 2, 'J.', 'Ramirez', '3B', 26),
+			--(207, 2, 'L.', 'Chisenhall', 'RF', 27),
+			--(208, 2, 'C.', 'Crisp', 'LF', 28),
+			--(209, 2, 'C.', 'Kluber', 'SP', 29),
 
-			-- Dodgers
-			(301, 3, 'A.', 'Gonzales', '1B', 34),
-			(302, 3, 'Y.', 'Grandal', 'C', 35),
-			(303, 3, 'J.', 'Pedersen', 'CF', 31),
-			(304, 3, 'C.', 'Seager', 'SS', 33),
-			(305, 3, 'A.', 'Toles', 'LF', 38),
-			(306, 3, 'J.', 'Turner', '3B', 36),
-			(307, 3, 'Y.', 'Puig', 'RF', 37),
-			(308, 3, 'C.', 'Utley', '2B', 32),
-			(309, 3, 'C.', 'Kershaw', 'SP', 39),
+			---- Dodgers
+			--(301, 3, 'A.', 'Gonzales', '1B', 34),
+			--(302, 3, 'Y.', 'Grandal', 'C', 35),
+			--(303, 3, 'J.', 'Pedersen', 'CF', 31),
+			--(304, 3, 'C.', 'Seager', 'SS', 33),
+			--(305, 3, 'A.', 'Toles', 'LF', 38),
+			--(306, 3, 'J.', 'Turner', '3B', 36),
+			--(307, 3, 'Y.', 'Puig', 'RF', 37),
+			--(308, 3, 'C.', 'Utley', '2B', 32),
+			--(309, 3, 'C.', 'Kershaw', 'SP', 39),
 
-			-- Blue Jays
-			(401, 4, 'J.', 'Bautista', 'RF', 41),
-			(402, 4, 'E.', 'Carrera', 'LF', 42),
-			(403, 4, 'J.', 'Donaldson', '3B', 43),
-			(404, 4, 'R.', 'Martin', 'C', 44),
-			(405, 4, 'K.', 'Pillar', 'CF', 45),
-			(406, 4, 'T.', 'Tulowitski', 'SS', 46),
-			(407, 4, 'J.', 'Smoak', '1B', 47),
-			(408, 4, 'D.', 'Travis', '2B', 48),
-			(409, 4, 'M.', 'Estrada', 'SP', 49);
+			---- Blue Jays
+			--(401, 4, 'J.', 'Bautista', 'RF', 41),
+			--(402, 4, 'E.', 'Carrera', 'LF', 42),
+			--(403, 4, 'J.', 'Donaldson', '3B', 43),
+			--(404, 4, 'R.', 'Martin', 'C', 44),
+			--(405, 4, 'K.', 'Pillar', 'CF', 45),
+			--(406, 4, 'T.', 'Tulowitski', 'SS', 46),
+			--(407, 4, 'J.', 'Smoak', '1B', 47),
+			--(408, 4, 'D.', 'Travis', '2B', 48),
+			--(409, 4, 'M.', 'Estrada', 'SP', 49);
 
 --ALTER TABLE [dbo].[batstats] DROP CONSTRAINT [FK__batstats__Player__286302EC]
 DROP TABLE if exists batstats;
 
 CREATE TABLE batstats (
 	BatStatID INTEGER NOT NULL PRIMARY KEY,
-	PlayerID INTEGER NOT NULL FOREIGN KEY REFERENCES roster(PlayerID),
+	PlayerID INTEGER NOT NULL, --FOREIGN KEY REFERENCES roster(PlayerID),
 	AtBats INTEGER,
 	Hits INTEGER,
 	Average FLOAT,
@@ -96,43 +105,71 @@ CREATE TABLE batstats (
 
 	);
 
-INSERT INTO batstats (BatStatID, PlayerID, AtBats, Hits, Average, HomeRuns, RunsBatIn, StrikeOuts)
-	VALUES	(11, 101, 68, 18, .265, 2, 8, 21),
-			(12, 102, 65, 20, .308, 3, 8, 21),
-			(13, 103, 39, 10, .256, 1, 6, 17),
-			(14, 104, 72, 18, .250, 3, 6, 17),
-			(15, 105, 65, 18, .277, 3, 10, 13),
-			(16, 106, 64, 13, .203, 3, 13, 13),
-			(17, 107, 48, 5, .104, 0, 1, 13),
-			(18, 108, 7, 0, .000, 0, 0, 3),
+	bulk insert batstats from 'C:\Users\sintal\Desktop\Project\Bulk\batstats.txt'
+with 
+(
+firstrow=2,
+datafiletype='char',
+fieldterminator=',',
+rowterminator='\n'
+);
+--INSERT INTO batstats (BatStatID, PlayerID, AtBats, Hits, Average, HomeRuns, RunsBatIn, StrikeOuts)
+--	VALUES	(11, 101, 68, 18, .265, 2, 8, 21),
+--			(12, 102, 65, 20, .308, 3, 8, 21),
+--			(13, 103, 39, 10, .256, 1, 6, 17),
+--			(14, 104, 72, 18, .250, 3, 6, 17),
+--			(15, 105, 65, 18, .277, 3, 10, 13),
+--			(16, 106, 64, 13, .203, 3, 13, 13),
+--			(17, 107, 48, 5, .104, 0, 1, 13),
+--			(18, 108, 7, 0, .000, 0, 0, 3),
 
-			(21, 201, 34, 5, .147, 1, 4, 9),
-			(22, 202, 61, 14, .230, 4, 8, 16),
-			(23, 203, 58, 18, .310, 2, 6, 16),
-			(24, 204, 52, 9, .173, 1, 3, 21),
-			(25, 205, 43, 8, .186, 3, 7, 15),
-			(26, 206, 56, 15, .268, 1, 3, 8),
-			(27, 207, 42, 9, .214, 1, 5, 13),
-			(28, 208, 26, 7, .269, 2, 4, 3),
+--			(21, 201, 34, 5, .147, 1, 4, 9),
+--			(22, 202, 61, 14, .230, 4, 8, 16),
+--			(23, 203, 58, 18, .310, 2, 6, 16),
+--			(24, 204, 52, 9, .173, 1, 3, 21),
+--			(25, 205, 43, 8, .186, 3, 7, 15),
+--			(26, 206, 56, 15, .268, 1, 3, 8),
+--			(27, 207, 42, 9, .214, 1, 5, 13),
+--			(28, 208, 26, 7, .269, 2, 4, 3),
 
-			(31, 301, 41, 8, .195, 2, 6, 8),
-			(32, 302, 28, 3, .107, 1, 3, 11),
-			(33, 303, 36, 9, .250, 1, 4, 14),
-			(34, 304, 44, 9, .205, 2, 4, 13),
-			(35, 305, 22, 8, .364, 0, 2, 3),
-			(36, 306, 35, 10, .286, 2, 8, 5),
-			(37, 307, 19, 4, .211, 0, 0, 3),
-			(38, 308, 28, 3, .107, 0, 2, 8),
+--			(31, 301, 41, 8, .195, 2, 6, 8),
+--			(32, 302, 28, 3, .107, 1, 3, 11),
+--			(33, 303, 36, 9, .250, 1, 4, 14),
+--			(34, 304, 44, 9, .205, 2, 4, 13),
+--			(35, 305, 22, 8, .364, 0, 2, 3),
+--			(36, 306, 35, 10, .286, 2, 8, 5),
+--			(37, 307, 19, 4, .211, 0, 0, 3),
+--			(38, 308, 28, 3, .107, 0, 2, 8),
 
-			(41, 401, 33, 6, .182, 2, 5, 12),
-			(42, 402, 33, 10, .303, 1, 3, 8),
-			(43, 403, 36, 15, .417, 1, 5, 7),
-			(44, 404, 33, 3, .091, 1, 1, 14),
-			(45, 405, 32, 3, .094, 1, 2, 6),
-			(46, 406, 35, 8, .229, 1, 5, 4),
-			(47, 407, 2, 0, .000, 0, 0, 2),
-			(48, 408, 12, 1, .083, 0, 0, 1);
+--			(41, 401, 33, 6, .182, 2, 5, 12),
+--			(42, 402, 33, 10, .303, 1, 3, 8),
+--			(43, 403, 36, 15, .417, 1, 5, 7),
+--			(44, 404, 33, 3, .091, 1, 1, 14),
+--			(45, 405, 32, 3, .094, 1, 2, 6),
+--			(46, 406, 35, 8, .229, 1, 5, 4),
+--			(47, 407, 2, 0, .000, 0, 0, 2),
+--			(48, 408, 12, 1, .083, 0, 0, 1);
 
+drop table if exists pitchstats;
+
+create table pitchstats(
+	playerID int,
+	Wins int,
+	Losses int,
+	ERA Float,
+	StrikeOuts int,
+	Walks int,
+	runsallowed int
+	);
+
+	bulk insert pitchstats from 'C:\Users\sintal\Desktop\Project\Bulk\pitchstats.txt'
+with 
+(
+firstrow=2,
+datafiletype='char',
+fieldterminator=',',
+rowterminator='\n'
+);
 --ALTER TABLE [dbo].[schedule] DROP CONSTRAINT [FK__schedule__Away_T__2C3393D0]
 --ALTER TABLE [dbo].[schedule] DROP CONSTRAINT [FK__schedule__Home_T__2B3F6F97]
 DROP TABLE if exists schedule;
